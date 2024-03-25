b98985e2d7c74243f65d19f813733e6e0978991a
Silvio Heinze <silvio.heinze@luftdaten.at>
2024-03-25 20:02:01 +0100

    change workshop list view to filter by end_date

--- code/workshops/views.py
+++ code/workshops/views.py
@@ -1,13 +1,17 @@
+from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from .models import Workshop
 
-
 class WorkshopListView(ListView):
     model = Workshop
     context_object_name = 'workshops'
     template_name = 'workshops/workshop_list.html'
+
+    def get_queryset(self):
+        # Return the Workshop queryset ordered by 'title' in ascending order
+        return Workshop.objects.all().filter(end_date__gt=timezone.now())
 
 
 class WorkshopDetailView(DetailView):
