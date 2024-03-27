2878235de3b5c60b12fabd14bdb3285edebd9721
Silvio Heinze <silvio.heinze@luftdaten.at>
2024-03-27 14:58:12 +0100

    add page arbeitsplatz

--- code/pages/urls.py
+++ code/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import ArbeitsplatzView, HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("arbeitsplatz/",ArbeitsplatzView.as_view(), name = "arbeitsplatz")
 ]
--- code/pages/views.py
+++ code/pages/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+class ArbeitsplatzView(TemplateView):
+    template_name = "arbeitsplatz.html"
