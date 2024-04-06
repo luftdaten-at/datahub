0308df64abb7fc890d2d7c3a287eac256a1e53c2
Silvio Heinze <silvio.heinze@luftdaten.at>
2024-04-06 23:45:57 +0200

    add description to FirmwareVersion model

--- code/devices/models.py
+++ code/devices/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class FirmwareVersion(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
