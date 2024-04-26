fb6707c03a1980405244508257ff170cf71d122f
Silvio Heinze <silvio.heinze@luftdaten.at>
2024-04-26 15:38:53 +0200

    add firmware to device model

--- code/devices/models.py
+++ code/devices/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class FirmwareVersion(models.Model):
+    """
+    Firmware version model.
+    """
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
 
@@ -8,13 +11,20 @@ class FirmwareVersion(models.Model):
         return self.name
 
 class DeviceModel(models.Model):
+    """
+    Device model model.
+    """
     name = models.CharField(max_length=255, unique=True, primary_key=True)
+    description = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.name
 
 
 class Device(models.Model):
+    """
+    Device model.
+    """
     name = models.CharField(max_length=4, unique=True, primary_key=True)
     model = models.ForeignKey(DeviceModel, on_delete=models.CASCADE, null=True)
     board_id = models.CharField(max_length=12, null=True, blank=True)
