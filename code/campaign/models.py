9c6f8b97c009bf279590e478e6766c03187d30a6
Silvio Heinze <silvio.heinze@luftdaten.at>
2024-05-01 13:16:36 +0200

    add autogeneration to id_token @ Campaign model

--- code/campaign/models.py
+++ code/campaign/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+import string
+import random
 
 class Campaign(models.Model):
     name = models.CharField(max_length=100)
@@ -18,3 +20,18 @@ class Campaign(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.id_token:
+            # Generate a unique ID for new workshops
+            self.id_token = self.generate_id_token()
+        super(Campaign, self).save(*args, **kwargs)
+
+    @staticmethod
+    def generate_id_token():
+        length = 8
+        # Ensure the generated ID is unique
+        while Campaign.objects.filter(id_token=id_token).exists():
+            # Generate a random string of letters and digits
+            id_token = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        return id_token
