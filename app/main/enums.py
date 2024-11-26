6ac83c3e13d76af786038ea204f84609cdbf4a9b
Silvio Heinze <silvioheinze@users.noreply.github.com>
2024-11-26 08:19:31 +0100

    add station detail view

---
+++ app/main/enums.py
@@ -0,0 +1,192 @@
+class Dimension():
+    PM0_1 = 1
+    PM1_0 = 2
+    PM2_5 = 3
+    PM4_0 = 4
+    PM10_0 = 5
+    HUMIDITY = 6
+    TEMPERATURE = 7
+    VOC_INDEX = 8
+    NOX_INDEX = 9
+    PRESSURE = 10
+    CO2 = 11
+    O3 = 12
+    AQI = 13
+    GAS_RESISTANCE = 14
+    TVOC = 15
+    NO2 = 16
+    SGP40_RAW_GAS = 17
+    SGP40_ADJUSTED_GAS = 18
+
+    # Dictionary für die Einheiten der Dimensionen
+    _units = {
+        PM0_1: "µg/m³",
+        PM1_0: "µg/m³",
+        PM2_5: "µg/m³",
+        PM4_0: "µg/m³",
+        PM10_0: "µg/m³",
+        HUMIDITY: "%",
+        TEMPERATURE: "°C",
+        VOC_INDEX: "Index",
+        NOX_INDEX: "Index",
+        PRESSURE: "hPa",
+        CO2: "ppm",
+        O3: "ppb",
+        AQI: "Index",
+        GAS_RESISTANCE: "Ω",
+        TVOC: "ppb",
+        NO2: "ppb",
+        SGP40_RAW_GAS: "Ω",
+        SGP40_ADJUSTED_GAS: "Ω",
+    }
+
+    _names = {
+        PM0_1: "PM0.1",
+        PM1_0: "PM1.0",
+        PM2_5: "PM2.5",
+        PM4_0: "PM4.0",
+        PM10_0: "PM10.0",
+        HUMIDITY: "Humidity",
+        TEMPERATURE: "Temperature",
+        VOC_INDEX: "VOC Index",
+        NOX_INDEX: "NOx Index",
+        PRESSURE: "Pressure",
+        CO2: "CO2",
+        O3: "Ozone (O3)",
+        AQI: "Air Quality Index (AQI)",
+        GAS_RESISTANCE: "Gas Resistance",
+        TVOC: "Total VOC",
+        NO2: "Nitrogen Dioxide (NO2)",
+        SGP40_RAW_GAS: "SGP40 Raw Gas",
+        SGP40_ADJUSTED_GAS: "SGP40 Adjusted Gas",
+    }
+
+    _sensor_community_names = {
+        PM0_1: "P01",
+        PM1_0: "P1",
+        PM2_5: "P2",
+        PM4_0: "P4",
+        PM10_0: "P10",
+        HUMIDITY: "humidity",
+        TEMPERATURE: "temperature",
+        PRESSURE: "pressure",
+        CO2: "co2_ppm",
+        O3: "ozone_ppb",
+        TVOC: "tvoc",
+        NO2: "no2_ppb",
+    }
+
+    @classmethod
+    def get_unit(cls, dimension_id: int) -> str:
+        """
+        Gibt die Einheit der angegebenen Dimension zurück.
+        :param dimension_id: Die ID der Dimension
+        :return: Die zugehörige Einheit oder 'Unknown', wenn keine Einheit vorhanden ist
+        """
+        return cls._units.get(dimension_id, "Unknown")
+
+    @classmethod
+    def get_name(cls, dimension_id: int) -> str:
+        """
+        Gibt den Namen der angegebenen Dimension zurück.
+        :param dimension_id: Die ID der Dimension
+        :return: Der zugehörige Name oder 'Unknown', wenn kein Name vorhanden ist
+        """
+        return cls._names.get(dimension_id, "Unknown")
+
+    @classmethod
+    def get_dimension_from_sensor_community_name(cls, sensor_community_name: str):
+        return {v:k for k, v in cls._sensor_community_names.items()}.get(sensor_community_name, None)
+
+
+class SensorModel():
+    SEN5X = 1
+    BMP280 = 2
+    BME280 = 3
+    BME680 = 4
+    SCD4X = 5
+    AHT20 = 6
+    SHT30 = 7
+    SHT31 = 8
+    AGS02MA = 9
+    SHT4X = 10
+    SGP40 = 11
+    DHT22 = 12
+    SDS011 = 13
+    SHT35 = 14
+    SPS30 = 15
+    PMS5003 = 16
+    PMS7003 = 17
+
+    _names = {
+        SEN5X: "SEN5X",
+        BMP280: "BMP280",
+        BME280: "BME280",
+        BME680: "BME680",
+        SCD4X: "SCD4X",
+        AHT20: "AHT20",
+        SHT30: "SHT30",
+        SHT31: "SHT31",
+        AGS02MA: "AGS02MA",
+        SHT4X: "SHT4X",
+        SGP40: "SGP40",
+        DHT22: "DHT22",
+        SDS011: "SDS011",
+        SHT35: "SHT35",
+        SPS30: "SPS30",
+        PMS5003: "PMS5003",
+        PMS7003: "PMS7003"
+    }
+
+    _manufacturer = {
+        SEN5X: "Sensirion",
+        BMP280: "Bosch Sensortec",
+        BME280: "Bosch Sensortec",
+        BME680: "Bosch Sensortec",
+        SCD4X: "Sensirion",
+        AHT20: "ASAIR",
+        SHT30: "Sensirion",
+        SHT31: "Sensirion",
+        AGS02MA: "ASAIR",
+        SHT4X: "Sensirion",
+        SGP40: "Sensirion",
+        DHT22: "ASAIR",
+        SDS011: "Nova Fitness",
+        SHT35: "Sensirion",
+        SPS30: "Sensirion",
+        PMS5003: "Plantower",
+        PMS7003: "Plantower"
+    }
+
+    @classmethod
+    def get_sensor_name(cls, sensor_model):
+        return cls._names.get(sensor_model, "Unknown Sensor")
+
+
+class LdProduct():
+    AIR_AROUND = 1
+    AIR_CUBE = 2
+    AIR_STATION = 3
+    AIR_BADGE = 4
+    AIR_BIKE = 5
+
+
+class Source():
+    LD = 1
+    LDTTN = 2
+    SC = 3
+
+    _names = {
+        LD: "Luftdaten.at",
+        LDTTN: "Luftdaten.at TTN LoRaWAN",
+        SC: "sensor.community"
+    }
+
+    @classmethod
+    def get_name(cls, source_id: int) -> str:
+        """
+        Gibt den Namen der angegebenen Source zurück.
+        :param source_id: Die ID der Source
+        :return: Der zugehörige Name oder 'Unknown', wenn kein Name vorhanden ist
+        """
+        return cls._names.get(source_id, "Unknown")
--- app/stations/views.py
+++ app/stations/views.py
@@ -2,6 +2,7 @@ import requests
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.conf import settings
+from main.enums import Dimension, SensorModel
 
 def StationDetailView(request, pk):
     # Beispiel API-URL, die von der Station-ID abhängt
@@ -10,14 +11,58 @@ def StationDetailView(request, pk):
     try:
         response = requests.get(api_url)
         response.raise_for_status()  # Prüft, ob die Anfrage erfolgreich war
-        station_data = response.json()  # Holt die Daten im JSON-Format
+        station_data = response.json()  # Daten im JSON-Format
+
+        # Überprüfen, ob "features" vorhanden und nicht leer sind
+        if station_data.get('features'):
+            feature = station_data['features'][0]  # Da nur eine Station erwartet wird
+            properties = feature.get('properties', {})
+            geometry = feature.get('geometry', {})
+            
+            # Extrahieren der relevanten Informationen
+            station_info = {
+                'id': properties.get('device'),
+                'time': properties.get('time'),
+                'height': properties.get('height'),
+                'coordinates': geometry.get('coordinates'),
+                'sensors': [],
+            }
+
+            # Verarbeitung der Sensoren
+            sensors = properties.get('sensors', [])
+            for sensor in sensors:
+                sensor_model_id = sensor.get('sensor_model')
+                sensor_model_name = SensorModel.get_sensor_name(sensor_model_id)
+
+                values = []
+                for value in sensor.get('values', []):
+                    dimension_id = value.get('dimension')
+                    dimension_name = Dimension.get_name(dimension_id)
+                    unit = Dimension.get_unit(dimension_id)
+                    val = value.get('value')
+
+                    values.append({
+                        'dimension_id': dimension_id,
+                        'dimension_name': dimension_name,
+                        'unit': unit,
+                        'value': val,
+                    })
+
+                station_info['sensors'].append({
+                    'sensor_model_id': sensor_model_id,
+                    'sensor_model_name': sensor_model_name,
+                    'values': values,
+                })
+        else:
+            raise Http404(f"Station mit ID {pk} nicht gefunden.")
+
     except requests.exceptions.HTTPError as err:
         raise Http404(f"Station mit ID {pk} nicht gefunden: {err}")
     except requests.exceptions.RequestException as e:
         return render(request, 'stations/error.html', {'error': str(e)})
 
     # Render die Daten im Template
-    return render(request, 'stations/detail.html', {'station': station_data})
+    return render(request, 'stations/detail.html', {'station': station_info})
 
 def StationListView(request):
     api_url = f"{settings.API_URL}/station/current?last_active=36000&output_format=csv"
