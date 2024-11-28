4e5ced88032bd17f1a87d630de63200b8d0e547b
Nik Sauer <nik.sauer@luftdaten.at>
2024-11-28 12:38:42 +0100

    request data from api and load into template

--- app/stations/views.py
+++ app/stations/views.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime, timezone, timedelta
+from collections import defaultdict
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.conf import settings
@@ -27,7 +29,28 @@ def StationDetailView(request, pk):
                 'coordinates': geometry.get('coordinates'),
                 'sensors': [],
             }
+            current_time = datetime.utcnow().replace(minute=0, second=0, microsecond=0) - timedelta(hours=1)
+            time_minus_48h = current_time - timedelta(hours=47)
+            formatted_current_time = current_time.isoformat(timespec='minutes')
+            formatted_time_minus_48h = time_minus_48h.isoformat(timespec='minutes')
+            # api query
+            api_sensor_data_48h = f"{settings.API_URL}/station/historical?station_ids={pk}&output_format=json&precision=hourly%20avg%20(one%20data%20point%20per%20hour)&start={formatted_time_minus_48h}&end={formatted_current_time}"
+            response = requests.get(api_sensor_data_48h)
+            response.raise_for_status()
+            data_48h = response.json()
+            dim_hour_val = defaultdict(lambda: [0 for _ in range(48)])
+            for data_hour in data_48h:
+                hour = datetime.fromisoformat(data_hour["time_measured"])
+                for dim_val in data_hour["values"]:
+                    dim = dim_val["dimension"]
+                    val = dim_val["value"]
+                    dim_hour_val[str(dim)][int((hour - time_minus_48h).total_seconds() // 3600)] = val
 
+            from pprint import pprint
+            pprint(dim_hour_val)
+            station_info["data_48h"] = dim_hour_val
+
+            '''
             # Verarbeitung der Sensoren
             sensors = properties.get('sensors', [])
             for sensor in sensors:
@@ -53,6 +76,7 @@ def StationDetailView(request, pk):
                     'sensor_model_name': sensor_model_name,
                     'values': values,
                 })
+            '''
         else:
             raise Http404(f"Station mit ID {pk} nicht gefunden.")
 
