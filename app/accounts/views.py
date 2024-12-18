9814689ebb9a20da02fe4205629c84407b7580a3
Nik Sauer <nik.sauer@luftdaten.at>
2024-12-18 13:18:27 +0100

    #71 handle_post_registration: add user to organizations if they have pending invitations

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,7 +1,9 @@
+import datetime
 from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import CustomUserCreationForm
+from campaign.models import OrganizationInvitation
 
 
 class SignupPageView(generic.CreateView):
@@ -10,4 +12,11 @@ class SignupPageView(generic.CreateView):
     template_name = "registration/signup.html"
 
     def handle_post_registration(self, user):
-        pass
+        # search for pending invitations
+        pending_invitations = OrganizationInvitation.objects.filter(email = user.email).all()
+        for invitation in pending_invitations:
+            # check if not already expired
+            if invitation.expiring_date > datetime.datetime.now():
+                invitation.organization.users.add(user)
+            # delete invitation
+            invitation.delete()
