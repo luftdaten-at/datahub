6b66af883881fc545216a5d9ebd5743a53af641a
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-03 10:37:39 +0100

    added fk current_organization to Device

--- app/devices/models.py
+++ app/devices/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from campaign.models import Room
+from campaign.models import Room, Organization
 
 
 class Device(models.Model):
@@ -16,6 +16,7 @@ class Device(models.Model):
     notes = models.TextField(null=True, blank=True)
     api_key = models.CharField(max_length=64, null=True)
     current_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='current_devices', null=True)
+    current_organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name='current_devices', null=True)
 
     def __str__(self):
         return self.id or "Undefined Device"  # Added fallback for undefined IDs
