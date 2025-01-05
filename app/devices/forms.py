6ef972911036e8520bee923ebfc3abd1d9dca461
Silvio Heinze <silvioheinze@users.noreply.github.com>
2025-01-05 23:54:57 +0100

    improve device detail view

--- app/devices/forms.py
+++ app/devices/forms.py
@@ -5,3 +5,15 @@ class DeviceForm(forms.ModelForm):
     class Meta:
         model = Device
         fields = '__all__'
+
+class DeviceNotesForm(forms.ModelForm):
+    class Meta:
+        model = Device
+        fields = ['notes']  # Only include the 'notes' field
+        widgets = {
+            'notes': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Enter notes here...',
+            }),
+        }
