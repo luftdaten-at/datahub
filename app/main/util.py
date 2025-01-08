512c95d130ce3bd2b65dc6fc4739a303d33361e5
Nik Sauer <nik.sauer@luftdaten.at>
2025-01-08 09:36:06 +0100

    added documentation

--- app/main/util.py
+++ app/main/util.py
@@ -3,6 +3,25 @@ import datetime
 from devices.models import Device, DeviceStatus
 
 def get_or_create_station(station_info: dict): 
+    '''
+    station_info dict: 
+    {
+        "time": "2025-01-07T11:23:23.439Z",
+        "device": "string",
+        "firmware": "string",
+        "model": 0,
+        "apikey": "123",
+        "battery": {
+            "voltage": 0,
+            "percentage": 0
+        }
+    }
+
+    creates a station_status entry with the information in station_info
+
+    return: Device if there exists a Device where Device.id = station_info['device']
+            else new device is created
+    '''
     station, created = Device.objects.get_or_create(
         id = station_info['device']
     )
@@ -21,6 +40,9 @@ def get_or_create_station(station_info: dict):
         battery_voltage = station_info['battery']['voltage'],
         battery_soc = station_info['battery']['percentage'],
     )
+
+    # update firmware field
+    station.firmware = station_info['firmware']
     
     station.save()
     station_status.save()
