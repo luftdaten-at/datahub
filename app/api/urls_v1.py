448d569063d500e61485b1b73d13bb5f9228c7c7
Silvio Heinze <silvioheinze@users.noreply.github.com>
2025-03-17 16:02:02 +0100

    fix workshop pk data url

--- app/api/urls_v1.py
+++ app/api/urls_v1.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('devices/', include(('api.urls_devices', 'devices'), namespace='devices')),
     path('workshops/data/add/', AirQualityDataAddView.as_view(), name='api-v1-air-quality-data-add'),
     path('workshops/<str:pk>/', WorkshopDetailView.as_view(), name='api-v1-workshop-detail'),
-    path('workshops/<str:pk>/data', WorkshopAirQualityDataView.as_view(), name='api-v1-workshop-data'),
+    path('workshops/<str:pk>/data/', WorkshopAirQualityDataView.as_view(), name='api-v1-workshop-data'),
 ]
