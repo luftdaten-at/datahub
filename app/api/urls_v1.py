ef48d2b5a627c4d18826bd50816fb38fa7277870
Silvio Heinze <silvioheinze@users.noreply.github.com>
2025-03-17 15:52:04 +0100

    fix api workshops/pk/data

--- app/api/urls_v1.py
+++ app/api/urls_v1.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import AirQualityDataAddView, WorkshopDetailView
+from .views import AirQualityDataAddView, WorkshopDetailView, WorkshopAirQualityDataView
 
 urlpatterns = [
     path('devices/', include(('api.urls_devices', 'devices'), namespace='devices')),
     path('workshops/data/add/', AirQualityDataAddView.as_view(), name='api-v1-air-quality-data-add'),
     path('workshops/<str:pk>/', WorkshopDetailView.as_view(), name='api-v1-workshop-detail'),
+    path('workshops/<str:pk>/data', WorkshopAirQualityDataView.as_view(), name='api-v1-workshop-data'),
 ]
