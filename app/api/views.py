b9900c9b547f33aabea858c08bb9feaf3cf02571
Nik Sauer <nik.sauer@luftdaten.at>
2025-05-02 13:48:40 +0200

    debug

--- app/api/views.py
+++ app/api/views.py
@@ -224,7 +224,11 @@ class CreateStationDataAPIView(APIView):
                     return JsonResponse({"status": "success"}, status=200)
 
             except Exception as e:
+                import traceback
+                print(traceback.format_exc())
                 return JsonResponse({"status": "error", "message": str(e)}, status=400)
 
         except Exception as e:
+            import traceback
+            print(traceback.format_exc())
             return JsonResponse({"status": "error", "message": str(e)}, status=400)
