651a49cf174f9b853dbadd38c2e176183df0b80a
Nik Sauer <nik.sauer@luftdaten.at>
2025-05-28 11:22:51 +0200

    changed order

--- app/devices/urls.py
+++ app/devices/urls.py
@@ -3,11 +3,11 @@ from .views import DeviceDetailView, DeviceEditView, DeviceListView, DeviceLogsC
 
 urlpatterns = [
     path('', DeviceListView.as_view(), name='devices-list'),
+    path('calibration/', calibrationView, name='calibration'),
     path('my/', DeviceMyView.as_view(), name='devices-my'),
     path('<str:pk>/', DeviceDetailView.as_view(), name='device-detail'),
     path('<str:pk>/delete/', DeviceEditView.as_view(), name='device-delete'),
     path('<str:pk>/edit/', DeviceEditView.as_view(), name='device-edit'),
     path('<str:pk>/edit-notes/', DeviceNotesUpdateView.as_view(), name='device-edit-notes'),
     path('<str:pk>/logs.csv', DeviceLogsCSVView.as_view(), name='device-logs-csv'),
-    path('calibration/', calibrationView, name='calibration'),
 ]
